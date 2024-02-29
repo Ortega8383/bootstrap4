a546f46f699f0f41abbb82a950f01730397b669f
Orhan Isik <isiko979@gmail.com>
2024-02-29 14:32:59 +0300

    Initial commit

---
+++ src/main/java/com/demo/bootstrap4/Bootstrap4Application.java
@@ -0,0 +1,13 @@
+package com.demo.bootstrap4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bootstrap4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bootstrap4Application.class, args);
+    }
+
+}
